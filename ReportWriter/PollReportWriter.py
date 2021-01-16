30e0423f3eba797fecd9ee46ba8c23b33b108e16
ayşe <ayse.krhsn@hotmail.com>
2021-01-17 02:41:29 +0300

    poll writer improvments!!

--- ReportWriter/PollReportWriter.py
+++ ReportWriter/PollReportWriter.py
@@ -8,40 +8,36 @@ class PollReportWriter:
         self.studentList = studentList
 
     def poll_report(self):
-        pollName = "ayse" #TODO we get pollName from pollList by using loop
-        wb = Workbook()
-        sheet1 = wb.add_sheet(pollName)
-
         i = 1
         while i < len(self.pollList):
-            sheet1.write(0, i, pollName)
-            i += 1
-        numofQuestionsColumn = i
-        sheet1.write(0, i, 'Number Of Questions')
-        i += 1
-        successRateColumn = i
-        sheet1.write(0, i, 'Success Rate')
-        i += 1
-        successPercentageColumn = i
-        sheet1.write(0, i, 'Success Percentage')
+            wb = Workbook()
+            sheet1 = wb.add_sheet(self.pollList[i].name)
+            m = 1
+            numofQuestionsColumn = m
+            sheet1.write(0, m, 'Number Of Questions')
+            m += 1
+            successRateColumn = m
+            sheet1.write(0, m, 'Success Rate')
+            m += 1
+            successPercentageColumn = m
+            sheet1.write(0, m, 'Success Percentage')
 
-        k = 0
-        while k < len(self.pollList):
             j = 1
             while j < len(self.studentList):
-                sheet1.write(j, numofQuestionsColumn, self.number_of_questions(self.pollList[k]))
+                sheet1.write(j, numofQuestionsColumn, self.number_of_questions(self.pollList[i]))
                 j += 1
-            k += 1
 
-        j = 1
-        while j < len(self.studentList):
-            sheet1.write(j, successRateColumn, self.success_rate())
-            j += 1
+            j = 1
+            while j < len(self.studentList):
+                sheet1.write(j, successRateColumn, self.success_rate())
+                j += 1
 
-        j = 1
-        while j < len(self.studentList):
-            sheet1.write(j, successPercentageColumn, self.success_percentage())
-            j += 1
+            j = 1
+            while j < len(self.studentList):
+                sheet1.write(j, successPercentageColumn, self.success_percentage())
+                j += 1
+            filename = "CSE3063_" + self.pollList.name + ".xlsx"
+            wb.save(filename)
 
     def number_of_questions(self, poll):
         numberOfQuestions = 0
