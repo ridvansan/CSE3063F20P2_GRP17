e46aeff20e7b3f23fa8954d9e141f26b4f9a501e
Rıdvan San <sanridvan@gmail.com>
2021-01-16 11:13:09 +0300

    Initial implementation of Student class

---
+++ Student.py
@@ -0,0 +1,8 @@
+
+class Student:
+
+    def __init__(self,studentID,name,surname,desc):
+        self.studentID = studentID
+        self.name = name
+        self.surname = surname
+        self.desc = desc
