38301d5e3ede22c7d575f5ba45d7d420beb4d02c
Efe Berke Erkeskin <efeberke1999@hotmail.com>
2021-01-16 17:53:52 +0300

    Poll reader class is in implement phase

--- inputReaders/PollReader.py
+++ inputReaders/PollReader.py
@@ -1,10 +1,20 @@
-import pandas as pd
+import csv
+from Poll import Poll
 
-
-class PollReader:
-
-    def __init__(self,filename):
+class InputHandler:
+    def __init__(self, filename):
         self.filename = filename
 
-    def returnPoll(self):
-        file = pd.read_csv(self.filename)
+    studentList = []
+
+    def readPolls(self, studentList):
+        self.studentList = studentList
+        with open(self.filename, 'r') as file:
+            lines = csv.reader(file)
+
+            for line in lines:
+              for student in studentList:
+                fullName = str(student.name + " " + student.surname).lower()
+                # if lowercased student name is in the studentList
+                if fullName == str(line[1]).lower():
+                    poll = Poll(self.filename)
