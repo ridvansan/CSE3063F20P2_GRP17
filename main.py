1d7ab0d98ca895b63fceee30e2db00cb1dde851b
Efe Berke Erkeskin <efeberke1999@hotmail.com>
2021-01-16 20:32:27 +0300

    Minor improvements

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-from inputReaders.KeyMaker import KeyMaker
 from inputReaders.StudentInputReader import StudentInputReader
+from inputReaders.PollReader import PollReader
+from models.PollAnswers import PollAnswers
+from inputReaders.KeyMaker import KeyMaker
 
 studentInputReader = StudentInputReader("assets/CES3063_Fall2020_rptSinifListesi.XLS")
 studentList = studentInputReader.getStudentList()
-#print(studentList)
+pollReader = PollReader("assets/CSE3063_20201123_Mon_zoom_PollReport.csv")
+pollReader.readPolls(studentList)
+studentList = pollReader.studentList
+
+for student in studentList:
+    print(student.name + " " + student.surname)
+    for answer in student.getPollAnswers():
+        for studentAnswer in answer.studentAnswers:
+            print(studentAnswer.answertext + ", ")
+    print("\n")
 
 
-keyMaker = KeyMaker('assets/answer_monday.xls')
-polls = keyMaker.makeKeys()
-print(polls)
-
-
+#k = KeyMaker('assets/answer_monday.xls')
+#k.makeKeys()
+#print(k)
 #input = InputHandler("poll.csv")
 #keys = KeyMaker("Keys.csv")
 
