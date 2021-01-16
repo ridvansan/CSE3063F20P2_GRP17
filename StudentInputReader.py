686873b0a057544e97bc2221f51d56417be0b2f0
Rıdvan San <sanridvan@gmail.com>
2021-01-16 11:23:37 +0300

    getStudentList method fully works

--- StudentInputReader.py
+++ StudentInputReader.py
@@ -3,7 +3,7 @@
 #LOC: -1
 
 import pandas as pd
-
+from Student import Student
 
 class StudentInputReader:
 
@@ -18,8 +18,14 @@ class StudentInputReader:
         file = file.reset_index(drop=True)
         file = file.drop([0])
         file = file.rename(columns={'Unnamed: 2': 'StudentID','Unnamed: 4': 'Name', 'Unnamed: 7': 'Surname','Unnamed: 10': 'Description',})
-        print(file.to_string())
-        # TODO create students from this list
-        return 0
+        studentList = []
+        ids = file.StudentID
+        names = file.Name
+        surnames = file.Surname
+        desc = file.Description
+        for i in range(1, len(file)):
+            studentList.append(Student(ids[i],names[i],surnames[i],desc[i]))
+        print(studentList)
+        return studentList
 
 
