49f4c5500710ff4a87f3281e9324b769d302bc62
ayşe <ayse.krhsn@hotmail.com>
2021-01-16 22:31:30 +0300

    student report writer improving

--- ReportWriter/StudentReportWriter.py
+++ ReportWriter/StudentReportWriter.py
@@ -1,24 +1,39 @@
-import pandas as pd
-from openpyxl import Workbook, load_workbook
+from xlwt import Workbook
+
 
 class StudentReportWriter:
-    wb = Workbook()
-    ws = wb.active
-    ws.title = "Student Report"
-    wb.save("CSE3063_studentReport.xlsx")
 
     def __init__(self, studentList):
         self.studentList = studentList
 
-    def writeOutputToFile(self):
-        pass
+    def write_output_to_file(self):
+        wb = Workbook()
+        sheet1 = wb.add_sheet('Student Report')
+
+        i = 0
+        while i < len(self.studentList):
+            sheet1.write(i, 0, self.studentList[i])
+            i += 1
+        i = 0
+        while i < len(self.studentList):
+            sheet1.write(i, 1, self.number_of_attendance_polls())
+            i += 1
+        i = 0
+        while i < len(self.studentList):
+            sheet1.write(i, 2, self.attendance_rate())
+            i += 1
+        i = 0
+        while i < len(self.studentList):
+            sheet1.write(i, 3, self.attendance_percentage())
+            i += 1
+        i = 0
+        wb.save("CSE3063_studentReport.xlsx")
 
     def number_of_attendance_polls(self):
-        pass
+        return 0
 
     def attendance_rate(self):
-        pass
+        return 0
 
     def attendance_percentage(self):
-        pass
-
+        return 0
