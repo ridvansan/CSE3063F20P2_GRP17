b1a2420b53361df970e04b863b509858dd67d5f9
Efe Berke Erkeskin <efeberke1999@hotmail.com>
2021-01-16 22:25:05 +0300

    Minor improvements!

--- inputReaders/PollReader.py
+++ inputReaders/PollReader.py
@@ -2,7 +2,6 @@ import csv
 
 from models.PollAnswer import PollAnswer
 from models.StudentAnswer import StudentAnswer
-from models.Poll import Poll
 
 class PollReader:
 
@@ -18,26 +17,17 @@ class PollReader:
 
             for line in lines:
                 for student in studentList:
-                    #Is comparison OK????????????????????
-                    fullName = str(student.name + " " + student.surname).lower()
-                    #remove numbers in the string
-                    #poll name represents student names in the polls.
-                    pollName = ''.join(i for i in str(line[1]).lower() if not i.isdigit())
-                    # if lowercased student name is in the studentList
-                    if fullName == pollName:
-                        print("aaa")
-                        #create pollAnswers
-                        pollAnswers = PollAnswer(self.filename, line[3])
 
-                        #questionlist = []
-                        #for i in range(4, len(line), 2):
-                        #   questionlist.append(line[i])
+                    fullName = str(student.name + " " + student.surname).lower()
+                    pollName = ''.join(i for i in str(line[1]).lower() if not i.isdigit())
+
+                    if fullName == pollName:
+                        pollAnswer = PollAnswer(self.filename, line[3])
 
                         for i in range(5, len(line), 2):
-                            #add answers to PollAnswers
-                            pollAnswers.addToStudentAnswers(StudentAnswer(line[i]))
-                        #add answersList to the corresponding Student
-                        student.addToPollAnswers(pollAnswers)
+                            pollAnswer.addToStudentAnswers(StudentAnswer(line[i]))
+
+                        student.addToPollAnswers(pollAnswer)
                         break
 
         file.close()
