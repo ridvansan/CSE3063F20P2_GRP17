912b9d10bd5d3974583c5acf02ee4babe0fc79e7
Rıdvan San <sanridvan@gmail.com>
2021-01-16 19:44:46 +0300

    Bug fixes

--- KeyMaker.py
+++ KeyMaker.py
@@ -14,7 +14,7 @@ class KeyMaker:
         polls = []
         for row in range(0,file.shape[0]):
             if pd.isnull(file.iat[row,1]):
-                polls.append(Poll((file.iat[row,0])))
+                polls.append(Poll(file.iat[row,0],[]))
                 i += 1
             else:
                 question = Question(file.iat[row,0])
