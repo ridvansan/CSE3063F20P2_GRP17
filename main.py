1082df035c38fffdded9182675ecde553edb01ad
Zeynep Alıcı <aliciizeynep@gmail.com>
2021-01-17 23:31:09 +0300

    set absences

--- main.py
+++ main.py
@@ -11,21 +11,23 @@ studentList = studentInputReader.getStudentList()
 keyMaker = KeyMaker('assets/keys')
 polls = keyMaker.makeKeysinDirectory()
 
-
 pollReader = PollReader("assets/pollReports")
 pollReader.readAnswersAtDirectory(studentList, polls)
-#pollReader.readQuestionFrequencies(polls)
-for poll in polls:
-    print(poll.name)
-    if not isinstance(poll, AttendancePoll):
-        poll.makeHistogram(poll)
-#attendanceReportWriter = StudentAttendanceReportWriter(studentList, polls)
-#attendanceReportWriter.write_output_to_file()
+# pollReader.readQuestionFrequencies(polls)
+# for poll in polls:
+# print(poll.name)
+# if not isinstance(poll, AttendancePoll):
+# poll.makeHistogram(poll)
+# attendanceReportWriter = StudentAttendanceReportWriter(studentList, polls)
+# attendanceReportWriter.write_output_to_file()
 
 #
 
-pollReader.setPollsOfStudents() # pollReaderın attributelarında her student ın girdiği pollar var
+pollReader.setPollsOfStudents()  # pollReaderın attributelarında her student ın girdiği pollar var
 
+for key, value in pollReader.pollsOfStudents.items():
+    currentStudentID = key.studentID
+    absencePollListOfCurrentStudent = pollReader.Diff(polls, value)
 
-a = PollReportWriter(studentList,polls)
+a = PollReportWriter(studentList, polls)
 a.quizReport()
