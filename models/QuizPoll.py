b05d4b2d99d5dd804d5e33baff36d7f22ad8603d
Zeynep Alıcı <aliciizeynep@gmail.com>
2021-01-18 13:08:35 +0300

    fix bugs

--- models/QuizPoll.py
+++
@@ -1,9 +0,0 @@
-from models.Poll import Poll
-
-
-class QuizPoll(Poll):
-    def __init__(self, name, questionList):
-        super().__init__(name, questionList)
-        self.name = name
-        self.questionList = questionList
-
