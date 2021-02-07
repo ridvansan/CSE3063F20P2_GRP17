3232e3efeec1fb61ec7682cfc5ce6e31242b1d11
Ahmet Akıl <ahmet.deathoftime@gmail.com>
2021-02-07 22:24:26 +0300

    Code refactorings

--- main.py
+++ main.py
@@ -27,26 +27,3 @@ studentAttendanceReportWriter.write_output_to_file(pollReader.pollDates)
 
 pollReportWriter = PollReportWriter(studentList, polls)
 pollReportWriter.quizReport()
-
-"""
-pollReader.setPollsOfStudents()
-for key, value in pollReader.pollsOfStudents.items():
-    currentStudentID = key.studentID
-    absencePollListOfCurrentStudent = pollReader.Diff(polls, value)
-
-    if len(absencePollListOfCurrentStudent) > 0:
-        print(f"student id: {currentStudentID} ")
-        for absencePoll in absencePollListOfCurrentStudent:
-            print(f"poll names: {absencePoll.name}")
-        print("\n")
-        print(f"number of absences of that student: {len(absencePollListOfCurrentStudent)}")
-
-pollReportWriter = PollReportWriter(studentList, polls)
-pollReportWriter.quizReport()
-
-
-
-for poll in polls:
-    if not isinstance(poll, AttendancePoll):
-        poll.makeHistogram()
-"""
